e6ecfd379d68b95a649ff03e4f0e6e66dd9e58d5
Unknown <enzo.billis@viacesi.fr>
2018-11-16 00:38:51 +0100

    Chappie try to learn badwords

--- commands/chappie.js
+++ commands/chappie.js
@@ -1,43 +1,53 @@
+let badwords = require("../assets/badwords")
+
 module.exports = class chappie {
 
-  static action(message){
-    let args = message.content.split(' ')
-    args.shift()
+    static action(message) {
+        let args = message.content.split(' ')
+        args.shift()
 
-    if(args[0] === "maman"){
-      message.channel.send({
-        files:["images/Chappie/chappie-mom.jpg"]
-      })
-        .then()
-        .catch(console.error)
-      return true
+        if (args[0] === "maman") {
+            message.channel.send({
+                files: ["images/Chappie/chappie-mom.jpg"]
+            })
+                .then()
+                .catch(console.error)
+            return true
+        }
+        else if (args[0] === "papa") {
+            message.channel.send({
+                files: ["images/Chappie/chappie-dad.jpg"]
+            })
+                .then()
+                .catch(console.error)
+            return true
+        }
+        else if (args[0] === "famille") {
+            message.channel.send({
+                files: ["images/Chappie/chappie-family.jpg"]
+            })
+                .then()
+                .catch(console.error)
+            return true
+        }
+        else if (args.includes("bye")) {
+            message.reply("Bisous °~°")
+            return true
+        }
+        else if (args.includes("coucou")) {
+            message.reply("Coucou toi °~°")
+            return true
+        }
+        else {
+            message.reply("Chappie pas comprendre....")
+        }
     }
-    else if(args[0] === "papa"){
-      message.channel.send({
-        files:["images/Chappie/chappie-dad.jpg"]
-      })
-        .then()
-        .catch(console.error)
-      return true
+
+    static chappieHateBadwords(message) {
+        // let badwordsFR = badwords.split("/");
+        // let messageArray = message.content.split(" ")
+        // return _.intersection(messageArray,badwordsFR).length > 0 ? message.reply("Chappie deteste les gros mots !") : false
+
+        // return message.content.match(badwords) ? true : false
     }
-    else if(args[0] === "famille"){
-      message.channel.send({
-        files:["images/Chappie/chappie-family.jpg"]
-      })
-        .then()
-        .catch(console.error)
-      return true
-    }
-    else if(args.includes("bye")){
-     message.reply("Bisous °~°")
-      return true
-    }
-    else if(args.includes("coucou")){
-      message.reply("Coucou toi °~°")
-      return true
-    }
-    else{
-      message.reply("Chappie pas comprendre....")
-    }
-  }
 }
